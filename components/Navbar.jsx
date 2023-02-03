3e9a74d7983fec19cdf792dc18d10ad102e8f952
migdalius <30595371+migdalius@users.noreply.github.com>
2023-02-03 17:00:44 +0100

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,11 +6,22 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <Image src="/img/telephone.png" alt="" width={50} height={50} />
+          <Image src="/img/telephone.png" alt="" width={30} height={30} />
         </div>
+        <div className={styles.texts}>ORDER NOW!</div>
+        <div className={styles.text}>012 345 678</div>
       </div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home Page</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>right</div>
     </div>
   );
 };
