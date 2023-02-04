2e759d34d317e3ec68d016c2cd36c1405f80ab0a
migdalius <30595371+migdalius@users.noreply.github.com>
2023-02-04 12:12:09 +0100

    static

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,24 +4,25 @@ import styles from "../styles/Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles.callButton}>
-          <Image src="/img/telephone.png" alt="" width={30} height={30} />
-        </div>
-        <div className={styles.texts}>ORDER NOW!</div>
-        <div className={styles.text}>012 345 678</div>
-      </div>
+      <div className={styles.item}></div>
       <div className={styles.item}>
         <ul className={styles.list}>
+          <Image src="/img/logo.png" alt="" width={130} height={35} />
           <li className={styles.listItem}>Home Page</li>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
+
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width={30} height={30} />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Featured from "@/components/Featured";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      home
+      <Featured />
     </div>
   );
 }
